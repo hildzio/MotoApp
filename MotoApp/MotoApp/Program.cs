b5e604ba38c1a08ec262349d88badd40b2eab628
Hildek <tomasz.hildebrand@gmail.com>
2023-01-25 21:03:16 +0100

    1

---
+++ MotoApp/MotoApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using MotoApp;
+
+var stack = new BasicStack();
+stack.Push(item: 4.5);
+stack.Push(item: 43);
+stack.Push(item: 333.6);
+
+double sum = 0.0;
+
+while (stack.Count > 0)
+{
+    double item = stack.Pop();
+    Console.WriteLine($"Item: {item}");
+    sum += item;
+}
+Console.WriteLine($"Sum: {sum}");

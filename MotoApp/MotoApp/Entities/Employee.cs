bc945660f26283b2b790a418dae3085627dbd41c
Hildek <tomasz.hildebrand@gmail.com>
2023-01-26 23:09:13 +0100

    po repo

---
+++ MotoApp/MotoApp/Entities/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MotoApp.Entities
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string? FirstName { get; set; }
+        // ? znaczy że zmienna może być null    
+        public override string ToString() => $"Id: {Id}, FirstName: {FirstName}";
+
+    }
+}
---
+++ MotoApp/MotoApp/Repositories/EmployRepository.cs
@@ -0,0 +1,33 @@
+﻿using MotoApp.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MotoApp.Repositories
+{
+    internal class EmployRepository
+    {
+        private readonly List<Employee> _employees = new();
+
+        public void Add(Employee employee)
+        {
+            employee.Id = _employees.Count + 1;
+            _employees.Add(employee);   
+        }
+
+        public void Save()
+        {
+            foreach(var employee in _employees) 
+            {
+                Console.WriteLine(employee);
+            }
+        }
+
+        public Employee GetById(int id)
+        { 
+            return _employees.Single(item => item.Id == id);
+        }
+    }
+}
